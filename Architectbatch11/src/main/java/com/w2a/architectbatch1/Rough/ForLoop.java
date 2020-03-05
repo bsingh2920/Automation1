81ca770327b19713ac240033baaf9c43c641a34e
SinghBhupi <bsingh89@dxc.com>
2020-03-05 10:22:52 +0530

    sdaffds

---
+++ Architectbatch11/src/main/java/com/w2a/architectbatch1/Rough/ForLoop.java
@@ -0,0 +1,17 @@
+package com.w2a.architectbatch1.Rough;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+	
+	int i=1;
+
+	for(;i<=10;++i){
+		
+		System.out.println(i);
+		}
+	System.out.println("-----------");
+	System.out.println(i);
+}
+}
